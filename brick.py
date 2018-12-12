850e844775bf4f18e69e64c3922574d38ac8e793
Jonathan <jonathan@iMac9.local>
2018-12-12 11:52:15 -0500

    Finishing the brick class

---
+++ brick.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+class Brick(pygame.sprite.Sprite):
+
+    def __init__(self, width, height, color):
+        # initialize sprite super class
+
+        # finish setting the class variables to the parameters
+        self.width = width
+        self.height = height
+        self.color = color
+
+        # Create a surface with the correct height and width
+        self.image = pygame.Surface((self.width, self.height))
+
+        # Get the rect coordinates
+        self.rect = self.image.get_rect()
+
+        # Fill the surface with the correct color
+        self.image.fill(self.color)
