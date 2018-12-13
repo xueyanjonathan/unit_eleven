4221e5be3e113779faecbb41bf9bd122fd85b2e1
Jonathan <jonathan@iMac9.local>
2018-12-13 13:35:18 -0500

    Making the block bounce off the walls

--- warmup.py
+++ warmup.py
@@ -4,7 +4,12 @@ import block
 
 
 pygame.init()
-main_window = pygame.display.set_mode((500, 500), 32, 0)
+
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
+X_SPEED = 3
+Y_SPEED = 5
+main_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 32, 0)
 pygame.display.set_caption("Animation")
 
 BLUE = (0, 0, 255)
@@ -25,8 +30,12 @@ while True:
             sys.exit()
 
     main_window.fill(WHITE)
-    my_block.rect.left += 3
-    my_block.rect.top += 3
+    my_block.rect.left += X_SPEED
+    my_block.rect.top += Y_SPEED
+    if my_block.rect.left <= 0 or my_block.rect.right >= WINDOW_WIDTH:
+        X_SPEED = -X_SPEED
+    if my_block.rect.top <= 0 or my_block.rect.bottom >= WINDOW_HEIGHT:
+        Y_SPEED = -Y_SPEED
     main_window.blit(my_block.image, my_block.rect)
     pygame.display.update()
 
